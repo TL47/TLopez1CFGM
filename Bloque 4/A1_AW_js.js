f13b8d53aea65deebdf54bfa8c273ef81e555002
TL47 <tlopdel0611@g.educaand.es>
2025-01-21 13:57:53 +0100

    updated

--- Bloque 4/A1_AW_js.js
+++ Bloque 4/A1_AW_js.js
@@ -1,6 +1,6 @@
-function validatePassword() {
-    let password = document.getElementById("password").value;
-    if (password.length < 7 || password.length > 20) {
-            alert("La clave debe tener entre 7 y 20 caracteres.");
+function comprobar() {
+        let clave = document.getElementById("pass").value;
+        if (clave.length < 7 || clave.length > 20) {
+                alert("La contraseña debe tener entre 7 y 20 caracteres.");
+            }
         }
-    }
---
+++ Bloque 4/A2_AW_js.js
@@ -0,0 +1,3 @@
+function cambiar(url) {
+    window.open(url)
+}
